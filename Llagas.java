5777a0faa6cd61de04046330051810e2c1f4aba0
llagasjames8-dotcom <llagasjames8@gmail.com>
2025-12-01 09:29:57 +0800

    Add files via upload

---
+++ Llagas.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Llagas here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Llagas
+{
+    public static void main (String [] args) {
+        for(int y=1 ; y<=5; y++){
+            int numbers= 10;
+            for(int x=1; x<=y; x++) {
+                System.out.print(number + " ") ;
+                number =2; 
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ james.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class james here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class james
+{
+ public static void main ( String [] args) {
+     int[] arr = {1, 7, 7, 3, 7, 4, 5, 6, 7, 7,};
+     int count = 0; 
+     for (int n : arr) if (n == 7) count++; 
+     System.out.println("7 appers" + count + "times") ;
+
+
+
+    }
+}
