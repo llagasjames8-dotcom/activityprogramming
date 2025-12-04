f06e18b2192f89bb9b42fc98e351ede70ce24c4a
llagasjames8-dotcom <llagasjames8@gmail.com>
2025-12-04 09:21:03 +0800

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Three here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Three
+{
+    public static void main ( String [] args) {
+        for( int i = 1; i <= 5; i++ ) { 
+          for (int j = 1; j <= i; j++) 
+  {
+         System.out.print("*"); 
+    }
+          System.out.println();
+      }
+    
+   }    
+
+}
