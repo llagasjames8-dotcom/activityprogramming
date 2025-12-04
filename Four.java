519d7d9d1b36b430b899564295f3a91cec54f20c
llagasjames8-dotcom <llagasjames8@gmail.com>
2025-12-04 09:28:19 +0800

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Four here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Four
+{
+    public static void main ( String [] args) {
+        int [] arr = {5,2,9,1,7};
+        
+        int min = arr [0];
+        int max = arr [0];
+        
+        for (int i = 1; i < arr . length; i ++) { 
+        
+        if (arr[i] < min )
+            min = arr[i];
+        if (arr[i]> max) 
+           max = arr [i]; 
+    }
+     System.out.println( "Smallest:" + min); 
+     System.out.println( "Largest:" + max) ;
+}
+}
